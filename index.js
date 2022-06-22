29c7e7b78bc45e40d7f59456daaafb88c8ede86c
cyphercode <97557732+matosky@users.noreply.github.com>
2022-06-22 20:55:58 +0100

    Delete index.js

--- index.js
+++
@@ -1,7 +0,0 @@
-const fullName = "Mathias Akari"
-
-let height = 1.87 + "m"
-
-let country = "Nigeria"
-
-console.log("Name: " + fullName + "\n" + "height: " + height + "\n" + "country: " + country)

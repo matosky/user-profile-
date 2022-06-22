fe2e598ad0433d90fa0a2a80fe134fd7cbf7471b
cyphercode <97557732+matosky@users.noreply.github.com>
2022-06-22 12:56:38 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,7 @@
+const fullName = "Mathias Akari"
+
+let height = 1.87 + "m"
+
+let country = "Nigeria"
+
+document.write("Name: " + fullName + "<br>" + "height: " + height + "<br>" + "country: " + country)
